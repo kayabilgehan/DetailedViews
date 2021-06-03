23ebeefe8cfa61d20ab57c62b5677cc7d1eaeedd
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-06-03 05:57:38 +0300

    Initial Commit

---
+++ DetailedViews/DetailedViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetailedViewsApp.swift
+//  DetailedViews
+//
+//  Created by Bilgehan KAYA on 3.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct DetailedViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
